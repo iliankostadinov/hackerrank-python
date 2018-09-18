83de4dfe33a4ac337b8c19f7441df3176d8ac134
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2018-09-18 14:53:16 +0300

    Final variant of if else problem

--- if-else.py
+++ if-else.py
@@ -3,7 +3,18 @@
 
 N = int(raw_input("Plase enter a number\n"))
 
-if N < 1 and N > 100:
-    print("You entered number out of range, please enter number between 1 and 100\n")
+if N < 1:
+    print "You entered number out of range, please enter number between 1 and 100\n"
+    N = int(raw_input("Plase enter a number\n"))
+elif N > 100:
+    print "You entered number out of range, please enter number between 1 and 100\n"
     N = int(raw_input("Plase enter a number\n"))
 
+if N % 2 != 0:
+    print "Weird"
+elif N < 6:
+    print "Not Weird"
+elif N < 21:
+    print "Weird"
+elif N > 20:
+    print "Not Weird"
