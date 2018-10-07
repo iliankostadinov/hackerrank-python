e8e40412bd4f4085e006b91ac3aa949a5e8200bf
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2018-10-07 11:57:30 +0300

    Only removed one unnecessery tab

--- lists.py
+++ lists.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    
+
     arr = [1,3,5]
     print("Please enter nuber of commands you want to execute")
     N = int(input())
     for i in range(0,N):
-        command = input()
+        command = raw_input().split()
         if command == 'print':
             print(arr)
 
