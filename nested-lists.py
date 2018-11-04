d01f6282bbb67c8ec3122ba9a9a3fb55a5cc78ab
ilian kostadinov <ilian@mail.kostadinovi.info>
2018-11-04 16:52:04 +0200

    Solution of nested lists

--- nested-lists.py
+++ nested-lists.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Given the names and grades for each student in a Physics class of students, store them in
+a nested list and print the name(s) of any student(s) having the second lowest grade.
+"""
+
 if __name__ == '__main__':
 
     nl = []
@@ -12,5 +17,30 @@ if __name__ == '__main__':
         nl.append(snl)
 
     nl.sort(key=lambda student: student[1])
+    
+    i = 1
+    if nl[0][1] == nl[1][1]:
+        listForPrint = [nl[0][0]]
+        while (nl[i-1][1] == nl[i][1]):
+            listForPrint.append(nl[i][0])
+            i += 1
+        if i == int(ns) - 1:
+            listForPrint.sort()
+        else:
+            listForPrint = [nl[i][0]]
+            while (nl[i][1] == nl[i+1][1]):
+                listForPrint.append(nl[i+1][0])
+                i += 1
+            listForPrint.sort()
 
-    print(nl)
+    else:
+            listForPrint = [nl[i][0]]
+            while (nl[i][1] == nl[i+1][1]):
+                listForPrint.append(nl[i+1][0])
+                i += 1
+            listForPrint.sort()
+    for names in listForPrint:
+        print(names)
+
+
+
